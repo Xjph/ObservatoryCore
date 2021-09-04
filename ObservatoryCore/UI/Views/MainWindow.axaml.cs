e68d8d06903b8c319e595ffd622b6ce5c5656c00
Xjph <archseraphim@gmail.com>
2021-09-04 18:34:55 -0230

    Save main window position/size.

--- ObservatoryCore/UI/Views/MainWindow.axaml.cs
+++ ObservatoryCore/UI/Views/MainWindow.axaml.cs
@@ -1,8 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using System.Collections.Generic;
-using System.Data;
 
 
 namespace Observatory.UI.Views
@@ -15,7 +13,17 @@ namespace Observatory.UI.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
-
+            Height = Properties.Core.Default.MainWindowSize.Height;
+            Width = Properties.Core.Default.MainWindowSize.Width;
+            Position = new PixelPoint(Properties.Core.Default.MainWindowPosition.X, Properties.Core.Default.MainWindowPosition.Y);
+            Closing += (object sender, System.ComponentModel.CancelEventArgs e) =>
+            {
+                var size = new System.Drawing.Size((int)System.Math.Round(Width), (int)System.Math.Round(Height));
+                var position = new System.Drawing.Point(Position.X, Position.Y);
+                Properties.Core.Default.MainWindowSize = size;
+                Properties.Core.Default.MainWindowPosition = position;
+                Properties.Core.Default.Save();
+            };
         }
 
         private void InitializeComponent()
