8094820ffd56be699c0c094c71d23df2323fb520
Xjph <archseraphim@gmail.com>
2021-11-24 19:42:32 -0330

    Add missing converter for micro resource trade

--- ObservatoryFramework/Files/Journal/Odyssey/TradeMicroResources.cs
+++ ObservatoryFramework/Files/Journal/Odyssey/TradeMicroResources.cs
@@ -1,5 +1,6 @@
 ﻿using Observatory.Framework.Files.ParameterTypes;
 using System.Collections.Immutable;
+using System.Text.Json.Serialization;
 
 namespace Observatory.Framework.Files.Journal
 {
@@ -7,6 +8,7 @@ namespace Observatory.Framework.Files.Journal
     {
         public ImmutableList<MicroResource> Offered { get; init; }
         public string Received { get; init; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public MicroCategory Category { get; init; }
         public int Count { get; init; }
         public ulong MarketID { get; init; }
