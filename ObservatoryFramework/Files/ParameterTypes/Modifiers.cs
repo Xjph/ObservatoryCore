c5f6258d95649664178e3822583ed0986286b8b1
Xjph <archseraphim@gmail.com>
2022-05-17 18:21:01 -0230

    Handle flip-flopping JSON type

--- ObservatoryFramework/Files/ParameterTypes/Modifiers.cs
+++ ObservatoryFramework/Files/ParameterTypes/Modifiers.cs
@@ -6,11 +6,31 @@ namespace Observatory.Framework.Files.ParameterTypes
     {
         public string Label { get; init; }
 
-        public double Value { get; init; }
+        public object Value 
+        { 
+            get
+            {
+                if (!string.IsNullOrEmpty(ValueString))
+                    return ValueString;
+                else
+                    return ValueNumeric;
+            }
+
+            init
+            {
+                if (value.GetType() == typeof(string))
+                    ValueString = value.ToString();
+                else
+                    ValueNumeric = (double)value;
+            }
+        }
 
         public double OriginalValue { get; init; }
 
         [JsonConverter(typeof(Converters.IntBoolConverter))]
         public bool LessIsGood { get; init; }
+
+        private double ValueNumeric;
+        private string ValueString;
     }
 }
