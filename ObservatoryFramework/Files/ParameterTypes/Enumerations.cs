b6d013b3ae5109f70236b7f998955abb5fb50030
Xjph <archseraphim@gmail.com>
2024-01-21 15:57:48 -0330

    add `collidedWithDamage` crime type

--- ObservatoryFramework/Files/ParameterTypes/Enumerations.cs
+++ ObservatoryFramework/Files/ParameterTypes/Enumerations.cs
@@ -82,7 +82,8 @@ namespace Observatory.Framework.Files.ParameterTypes
         onFoot_failureToSubmitToPolice,
         onfoot_dataTransfer,
         onFoot_carryingStolenGoods,
-        onFoot_eBreachUse
+        onFoot_eBreachUse,
+        collidedWithDamage
     }
 
     public enum LimpetDrone
