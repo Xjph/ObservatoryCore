0f67dbf8831fd8577ae1c7c499a77a6f8eac3c2c
Fred K <mr.fredk@gmail.com>
2024-07-03 23:13:10 -0400

    fix(core): rework core panel layout to fix scrolling
    After some of the recent work, plugin list scrolling *and* core settings panel scrolling stopped working. While those fixes were relatively trivial, it leads to nested scrolling (ListView + core panel scrolling) which is ... blah.
    
    Soo, did a bit of rearranging:
    * Added a Splitter between the plugin list + buttons and the rest of the core settings panels. This allows the user to expand the plugin list view to show more plugins. This also means only 1 scrollbar is necessary in the top half of the view (because the bottom half of the content can scroll independently). The splitter position is saved across app restarts and has been included in default themes to be visible.
    * Resized the core settings panels to be consistently sized and added a flow layout panel so regardless of the orientation of the main window view, they'll flex to the shape of the window and largely be always visible/accessible.
    
    In the process, I made the unfortunate discovery of the following fact: Copy & Pasting controls where the event handler code for those controls is in a partial (so in `CoreForm.Settings.cs`) results in those controls losing their event handler wiring in `CoreForm.Designer.cs`. So far as I can tell, I got them all back again, but in-case something is off, check there first (but be warned, the `CoreForm.Designer.cs` diff is awful..
    
    Also, there's a bonus value range clamp fix for VoiceSpeedSlider.
    
    Also, added some try-catches to handle some seemingly benign GDI errors in the new tab control drawitem event handler.

--- ObservatoryCore/Properties/Core.Designer.cs
+++ ObservatoryCore/Properties/Core.Designer.cs
@@ -12,7 +12,7 @@ namespace Observatory.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     internal sealed partial class Core : global::System.Configuration.ApplicationSettingsBase {
         
         private static Core defaultInstance = ((Core)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Core())));
@@ -331,6 +331,18 @@ namespace Observatory.Properties {
             }
             set {
                 this["SavedThemes"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("250")]
+        public int CoreSplitterDistance {
+            get {
+                return ((int)(this["CoreSplitterDistance"]));
+            }
+            set {
+                this["CoreSplitterDistance"] = value;
             }
         }
     }
