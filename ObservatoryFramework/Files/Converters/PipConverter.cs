727ca4f5545ff1e8aa8b06ec4ccb1545cf96f62c
Xjph <archseraphim@gmail.com>
2024-11-19 20:39:07 -0330

    chore: removing tuples from framework

--- ObservatoryFramework/Files/Converters/PipConverter.cs
+++ ObservatoryFramework/Files/Converters/PipConverter.cs
@@ -1,18 +1,19 @@
-﻿using System.Text.Json;
+﻿using Observatory.Framework.Files.ParameterTypes;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Observatory.Framework.Files.Converters
 {
-    class PipConverter : JsonConverter<(int Sys, int Eng, int Wep)>
+    class PipConverter : JsonConverter<Pips>
     {
-        public override (int Sys, int Eng, int Wep) Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override Pips Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             int[] values = (int[])JsonSerializer.Deserialize(ref reader, typeof(int[]));
 
-            return (Sys: values[0], Eng: values[1], Wep: values[2]);
+            return new Pips() { Sys = values[0], Eng = values[1], Wep = values[2] };
         }
 
-        public override void Write(Utf8JsonWriter writer, (int Sys, int Eng, int Wep) value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer, Pips value, JsonSerializerOptions options)
         {
             throw new NotImplementedException();
         }
---
+++ ObservatoryFramework/Files/ParameterTypes/Pips.cs
@@ -0,0 +1,9 @@
+﻿namespace Observatory.Framework.Files.ParameterTypes
+{
+    public class Pips
+    {
+        public int Sys { get; init; }
+        public int Eng { get; init; }
+        public int Wep { get; init; }
+    }
+}
