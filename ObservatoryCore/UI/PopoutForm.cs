99acd7eee679a3d97301c7e16dd923d93bdafd5e
Xjph <archseraphim@gmail.com>
2024-11-20 23:58:54 -0330

    fix(core): save popout position as json

--- ObservatoryCore/UI/PopoutForm.cs
+++ ObservatoryCore/UI/PopoutForm.cs
@@ -141,31 +141,25 @@ namespace Observatory.UI
 
         private void RestorePosition()
         {
-            var savedPositions = Properties.Core.Default.PopoutLocation;
-            if (savedPositions != null)
+            Dictionary<string, PopoutPosition> savedPositions;
+            try
             {
-                foreach (var position in savedPositions)
-                {
-                    if (position?.StartsWith(Text) ?? false)
-                    {
-                        var values = position.Split('\u001f');
-                        if (values.Length == 5
-                            && int.TryParse(values[1], out int locationX)
-                            && int.TryParse(values[2], out int locationY)
-                            && int.TryParse(values[3], out int sizeW)
-                            && int.TryParse(values[4], out int sizeH))
-                        {
+                savedPositions = JsonSerializer.Deserialize<Dictionary<string, PopoutPosition>>
+                    (Properties.Core.Default.PopoutLocation) ?? [];
+            }
+            catch
+            {
+                savedPositions = [];
+            }
 
-                            Point savedLocation = new(locationX, locationY);
-                            Size savedSize = new(sizeW, sizeH);
-                            if (WithinScreenBounds(savedLocation, savedSize))
-                            {
-                                Size = savedSize;
-                                Location = savedLocation;
-                            }
-                        }
-                        break;
-                    }
+            if (savedPositions.TryGetValue(Text, out PopoutPosition? value) && value != null)
+            {
+                Point savedLocation = new(value.X, value.Y);
+                Size savedSize = new(value.Width, value.Height);
+                if (WithinScreenBounds(savedLocation, savedSize))
+                {
+                    Size = savedSize;
+                    Location = savedLocation;
                 }
             }
         }
@@ -191,23 +185,21 @@ namespace Observatory.UI
 
         private void SavePosition()
         {
-            var positionString = $"{Text}\u001f{Location.X}\u001f{Location.Y}\u001f{Width}\u001f{Height}";
-            bool found = false;
-            var savedPositions = Properties.Core.Default.PopoutLocation;
-            for (int i = 0; i < savedPositions?.Count; i++)
+            var position = new PopoutPosition(Location.X, Location.Y, Width, Height);
+
+            Dictionary<string, PopoutPosition> savedPositions;
+            try
             {
-                if (savedPositions[i]?.StartsWith(Text) ?? false)
-                {
-                    savedPositions[i] = positionString;
-                    found = true;
-                }
+                savedPositions = JsonSerializer.Deserialize<Dictionary<string, PopoutPosition>>
+                    (Properties.Core.Default.PopoutLocation) ?? [];
             }
-            if (!found)
+            catch
             {
-                savedPositions ??= [];
-                savedPositions.Add(positionString);
+                savedPositions = [];
             }
-            Properties.Core.Default.PopoutLocation = savedPositions;
+            savedPositions[Text] = position;
+
+            Properties.Core.Default.PopoutLocation = JsonSerializer.Serialize(savedPositions);
             SettingsManager.Save();
         }
 
