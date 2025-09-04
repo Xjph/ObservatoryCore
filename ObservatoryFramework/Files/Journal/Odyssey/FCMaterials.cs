7c26ce058b3da9eb231de22be44a4df1a6590f8a
Xjph <archseraphim@gmail.com>
2025-09-04 19:53:03 -0230

    fix: duplicate class

--- ObservatoryFramework/Files/Journal/Odyssey/FCMaterials.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Observatory.Framework.Files.Journal
-{
-    public class FCMaterials : JournalBase
-    {
-        public ulong MarketID { get; init; }
-        public string CarrierName { get; init; }
-        public string CarrierID { get; init; }
-    }
-}
