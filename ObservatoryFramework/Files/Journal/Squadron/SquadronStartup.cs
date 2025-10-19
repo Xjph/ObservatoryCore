ac4cd7707d18cb77365cb14eef98c9f6a495a4c6
F K <54195004+fredjk-gh@users.noreply.github.com>
2025-10-19 13:43:33 -0400

    cleanup(framework): Remove duplicate SquadronID from SquadronStartup (#229)
    The property hides the base-class property. May have caused me a bit of grief with using it.
    
    And add missing documentation to ObservatoryFramework.xml.

--- ObservatoryFramework/Files/Journal/Squadron/SquadronStartup.cs
+++ ObservatoryFramework/Files/Journal/Squadron/SquadronStartup.cs
@@ -5,6 +5,5 @@
         public int CurrentRank { get; init; }
         public string CurrentRankName { get; init; }
         public string CurrentRankName_Localised { get; init; }
-        public ulong SquadronID { get; init; }
     }
 }
