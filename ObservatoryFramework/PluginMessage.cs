e4f8ecb32561e06241e7dec23785524dbb2c4ed9
Fred K <mr.fredk@gmail.com>
2026-01-04 01:40:40 -0500

    fix(core,framework): Fix loss of message ID which breaks inreplyto id
    `PluginMessage`s are cloned internally to prevent/reduce tampering, but every clone had a different Guid (as there was no constructor param for it). However, this also breaks in-reply-to expectations. (ie. if the sender captures the message id of the message it sends, it will get a different in-reply-to id back every time).
    
    To remedy this, I:
    * Implemented the `ICloneable` interface in `PluginMessage` and moved the cloning logic out of `PluginEventHandler`, and into the `PluginMessage.Clone()` method.
    * Added a second, private constructor with a parameter for MessageID that can be used by the Clone() implementation to preserve the original message id. The original public constructor calls the new one and passes `Guid.NewGuid()` to the new parameter, preserving previous behaviour of a new Guid for new messages.
    * Updated `PluginEventHandler` to simply call the `Clone()` method now.
    
    The messageid -> in-reply-to id chain now works as expected.

--- ObservatoryFramework/PluginMessage.cs
+++ ObservatoryFramework/PluginMessage.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Represents a message sent between plugins in the Observatory Framework.
     /// </summary>
-    public class PluginMessage
+    public class PluginMessage : ICloneable
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="PluginMessage"/> class.
@@ -12,13 +12,27 @@
         /// <param name="messagePayload">A dictionary containing the payload of the message as key-value pairs.</param>
         /// <param name="inReplyTo">An optional identifier of the message for which this message is a reply.</param>
         public PluginMessage(string messageType, Dictionary<string, object> messagePayload = null, Guid? inReplyTo = null)
+            : this(Guid.NewGuid(), messageType, messagePayload, inReplyTo)
         {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <csee cref="PluginMessage"/> including the messageId for use by the
+        /// Clone() implementation to preserve the MessageID in the cloned object.
+        /// </summary>
+        /// <param name="messageId"></param>
+        /// <param name="messageType"></param>
+        /// <param name="messagePayload"></param>
+        /// <param name="inReplyTo"></param>
+        private PluginMessage(Guid messageId, string messageType, Dictionary<string, object> messagePayload = null, Guid? inReplyTo = null)
+        {
+            _messageId = messageId;
             InReplyTo = inReplyTo;
             MessageType = messageType;
             MessagePayload = messagePayload ?? [];
         }
 
-        private readonly Guid _messageId = Guid.NewGuid();
+        private readonly Guid _messageId;
 
         /// <summary>
         /// Gets the unique identifier for the message.
@@ -39,5 +53,17 @@
         /// Gets the payload of the message as a dictionary of key-value pairs.
         /// </summary>
         public Dictionary<string, object> MessagePayload { get; init; }
+
+        public object Clone()
+        {
+            // Clone the message properties and create new message object to prevent bad actor plugins from modifying in-flight
+            var clonedPayload = MessagePayload.ToDictionary(item => item.Key, item => item.Value);
+            var clonedMessageType =
+                new Span<char>(MessageType.ToCharArray());
+            var clonedReplyId = new Guid((InReplyTo ?? Guid.Empty).ToString());
+            var clonedMsgId = new Guid(MessageId.ToString());
+
+            return new PluginMessage(clonedMsgId, clonedMessageType.ToString(), clonedPayload, clonedReplyId);
+        }
     }
 }
